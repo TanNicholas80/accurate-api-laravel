1ed7e6a6a7eebffade23df66311aebd04a9098d2
TanNicholas80 <tannicholas54@gmail.com>
2025-05-13 11:43:44 +0700

    Update API Laravel Service Stock Accurate

--- app/Http/Controllers/AccurateController.php
+++ app/Http/Controllers/AccurateController.php
@@ -10,15 +10,20 @@ class AccurateController extends Controller
     /**
      * Get Accurate Stock
      */
-    public function GetAllStockAccurate()
+    public function GetAllStockAccurate(Request $request)
     {
+        // Ambil halaman dari query string (?page=X) atau dari parameter URL segment
+        $page = $request->input('page', 1); // Default ke halaman 1 jika tidak disediakan
+
         $authToken = config('services.accurate.auth_token');
         $sessionToken = config('services.accurate.session_token');
-        
+
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $authToken,
             'X-Session-ID' => $sessionToken
-        ])->get('https://public.accurate.id/accurate/api/item/list-stock.do');
+        ])->get('https://public.accurate.id/accurate/api/item/list-stock.do', [
+            'sp.page' => $page
+        ]);
 
         if ($response->successful()) {
             return response()->json([
